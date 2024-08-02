eaf4ddcd7efa74ce22972a2766151790530aaf53
Deep_Lajpal <lajpaldeep@gmail.com>
2024-08-02 13:36:30 +0530

    chore: Update header to include cart icon and total count

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,22 @@ import { FaCartShopping } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 
+
 const Header = () => {
-    const [cartId, setCartId] = useState(0);
 
     return (
         <div className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <div className={styles.brandNameContainer}> <Link to="/" className={styles.brandName}>E-Commerce Website</Link> </div>
                 <div className={styles.profileIconContainer}> <CgProfile /> </div>
-                <div className={styles.cartIconContainer}> <Link to={`/cart/${cartId}`} className={styles.cartIcon}> <FaCartShopping /> </Link> </div>
+                <div className={styles.cartContainer}>
+                    <div className={styles.cartIconContainer}>
+                        <Link to={`/cart/${Math.floor(Math.random() * 10)}`} className={styles.cartIcon}> <FaCartShopping /> </Link>
+                    </div>
+                    <div className={styles.cartCircleContainer}><span className={styles.cartTotal}>5</span></div>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
